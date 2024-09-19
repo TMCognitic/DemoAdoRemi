5b72fe0265fbc31e548d15da7cc0e4be2ec28865
Thierry Morre <thierry.morre@cognitic.be>
2024-09-19 15:41:26 +0200

    Init

---
+++ DemoOO/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace DemoOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Observable observable = new Observable();
+            observable.Notify += (o) => Console.WriteLine($"La valeur a changé : {o.X}");
+  
+            observable.X = 5;
+            observable.X = 7;
+            observable.X = 0;
+        }        
+    }
+
+    class Observable
+    {
+        public event Action<Observable>? Notify;
+
+        private int _x;
+
+        public int X
+        {
+            get
+            {
+                return _x;
+            }
+
+            set
+            {
+                if(X != value)
+                {
+                    _x = value;
+                    Notify?.Invoke(this);
+                }
+            }
+        }
+    }
+
+    
+}
+
